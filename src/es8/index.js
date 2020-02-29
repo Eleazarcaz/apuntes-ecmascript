d98d95a3ae0ee28a2f0593dd245e084095eccb67
Eleazar <eleazarcaz@outlook.com>
2020-02-29 13:39:58 -0600

    padStart padEnd

--- src/es8/index.js
+++ src/es8/index.js
@@ -10,3 +10,9 @@ console.log(elementos, elementos.length);
 // devuelve un array con los valores de las propiedades
 const valores = Object.values(puestos);
 console.log(valores, valores.length);
+// ---------------------------------------------
+
+const fruta = 'platano';
+
+console.log(fruta.padStart(fruta.length + 5, 'rico '));
+console.log(fruta.padEnd(fruta.length + 5, ' rico'));
