5315d5c724a5bd2ed7ebb6a283c773909932a55f
Eleazar <eleazarcaz@outlook.com>
2020-02-29 14:06:35 -0600

    async-await

--- src/es8/index.js
+++ src/es8/index.js
@@ -16,3 +16,32 @@ const fruta = 'platano';
 
 console.log(fruta.padStart(fruta.length + 5, 'rico '));
 console.log(fruta.padEnd(fruta.length + 5, ' rico'));
+// -----------------------------------------------------
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    false
+      ? setTimeout(() => resolve('hello world'), 3000)
+      : reject('Salio algo mal');
+  });
+};
+
+helloWorld()
+  .then((dato) => {
+    console.log(dato);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// mejor manera para escribir asyn-await
+const helloAsync = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+helloAsync();
