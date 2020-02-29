fd9e6db00b668031bb92ff9d46f7523d07c49770
Eleazar <eleazarcaz@outlook.com>
2020-02-29 14:36:37 -0600

    Mejoras en regexdata (expresiones regulares)

--- src/es9/index.js
+++ src/es9/index.js
@@ -20,3 +20,11 @@ helloWorld()
   .catch((error) => console.error(error))
   .finally(() => console.log('termino'));
 // --------------------------------------------
+
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2020-02-29');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
